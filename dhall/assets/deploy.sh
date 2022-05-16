3e6b0b6e4376b00a96ec4318f68634b90682dd2d
Pontus <persman@kth.se>
2022-05-16 13:02:21 +0200

    working?

--- dhall/assets/deploy.sh
+++ dhall/assets/deploy.sh
@@ -1,6 +1,5 @@
 #/bin/bash
 
-echo "hello"
-#tar --extract --bzip2 --file dhall-json-1.7.10-x86_64-linux.tar.bz2
-#cp ./bin/dhall-to-{json,yaml} /usr/local/bin
-printf 'Hello?'
+wget -c https://github.com/dhall-lang/dhall-haskell/releases/download/1.41.1/dhall-json-1.7.10-x86_64-linux.tar.bz2
+tar --extract --bzip2 --file dhall-json-1.7.10-x86_64-linux.tar.bz2
+cp ./bin/dhall-to-{json,yaml} /usr/local/bin
