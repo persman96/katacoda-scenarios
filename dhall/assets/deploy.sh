2cc63ea6868e9a9f2470f6778b6ec78e429cc2de
Pontus <persman@kth.se>
2022-05-16 11:59:54 +0200

    testing script

--- dhall/assets/deploy.sh
+++ dhall/assets/deploy.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-echo 'Hello?'
-tar --extract --bzip2 --file dhall-json-1.7.10-x86_64-linux.tar.bz2
-cp ./bin/dhall-to-{json,yaml} /usr/local/bin
+echo "hello"
+#tar --extract --bzip2 --file dhall-json-1.7.10-x86_64-linux.tar.bz2
+#cp ./bin/dhall-to-{json,yaml} /usr/local/bin
 printf 'Hello?'
---
+++ dhall/background.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/usr/local/bin/deploy.sh
+
+echo "done"
