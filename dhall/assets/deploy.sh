fdeefc8c07417700880f0cd45d3d8cb280b9c91c
Pontus <persman@kth.se>
2022-05-16 10:49:38 +0200

    testing

--- dhall/assets/deploy.sh
+++ dhall/assets/deploy.sh
@@ -1,5 +1,6 @@
-cd /root/
+#/bin/bash
 
-tar --extract --bzip2 --file dhall-json-*-x86_64-linux.tar.bz2
+printf 'Hello?'
+tar --extract --bzip2 --file dhall-json-1.7.10-x86_64-linux.tar.bz2
 cp ./bin/dhall-to-{json,yaml} /usr/local/bin
 printf 'Hello?'
